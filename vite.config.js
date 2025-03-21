0126eaac89e3bd4d6afec1a740edc3ef6ba552bc
“Janette <mujica.janette@gmail.com>
2025-03-21 08:35:52 -0400

    Fix vite.config.js syntax error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-Copierimport { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/CAFY_prototype/'
-})
+	plugins: [react()],
+	base: '/CAFY_prototype/',
+});
